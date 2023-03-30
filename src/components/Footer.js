c86c1b22ea9262d4e9a1b1aaeec3348f4fcd51e7
Daniel Andrade <daniel.andrade.leon@gmail.com>
2023-03-29 23:13:09 -0500

    AldanStore 1.0

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(props) {
+    return <div style={{
+        textAlign: "center",
+        height:"30vh",
+        fontFamily: "Inter"
+    }}>Powered by Aldan Industries ©2023</div>;
+}
+
+export default Footer;
