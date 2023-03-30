44fe6a288ffe8f15a7ac3744c66aae17aee6dad3
Daniel Andrade <daniel.andrade.leon@gmail.com>
2023-03-29 23:13:09 -0500

    Primera Version Aldan Store

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,13 @@
+import { GiShoppingCart } from "react-icons/gi";
+
+const CartWidget = ()=> {
+    return (
+        <div style= {{
+            fontSize: "50px",
+        }}>
+           < GiShoppingCart />
+        </div>
+    )
+}
+
+export default CartWidget;
---
+++ src/components/Section.js
@@ -0,0 +1,17 @@
+
+const Section = ({greeting}) => {
+    return (
+        <div style={{
+                textAlign: "center",
+                height:"30vh",
+                fontFamily: "Inter"
+             }}>
+
+            <h2>{greeting}</h2>
+            
+        </div>
+    )
+    
+    }
+
+export default Section;   
